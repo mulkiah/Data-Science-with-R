b562a19d8c7b4c34c3dfe8e2d5582cdf2902b004
Mulkiah <56537756+mulkiah@users.noreply.github.com>
2021-10-20 09:02:31 +0800

    Update code

--- Project Analisa Klasifikasi Pinjaman untuk Sektor UMKM.R
+++ Project Analisa Klasifikasi Pinjaman untuk Sektor UMKM.R
@@ -54,3 +54,52 @@ ggcorrplot(round(cor(M),1),
            title="Correlogram of Data Nasabah",
            ggtheme=theme_bw)
 
+# Pemilihan fitur/independent variabel/input
+colnames(data_reduce)
+data_select = data_reduce[,c("KARAKTER","KONDISI_USAHA","KONDISI_JAMINAN","STATUS","KEWAJIBAN","OSL","KOLEKTIBILITAS","REKOMENDASI_TINDAK_LANJUT")]
+
+# Transformasi Data
+data_non_na = na.omit(data_select)
+data_select_new = data_select
+data_select_new$KEWAJIBAN = scale(data_select_new$KEWAJIBAN)[,1]
+data_select_new$OSL = scale(data_select_new$OSL)[,1]
+data_select_new$KEWAJIBAN = cut(data_select_new$KEWAJIBAN,breaks=c(-0.354107,5,15,30))
+data_select_new$KEWAJIBAN = as.factor(data_select_new$KEWAJIBAN)
+data_select_new$OSL = cut(data_select_new$OSL,breaks=c(-0.60383,3,10,15))
+data_select_new$OSL = as.factor(data_select_new$OSL)
+data_select_new = na.omit(data_select_new)
+
+# Training Data
+library(caret)
+index = createDataPartition(data_select_new$REKOMENDASI_TINDAK_LANJUT,p=.95, list = FALSE)
+train = data_select_new[index,]
+test = data_select_new[-index,]
+
+# Pemodelan/Modelling
+train2 = train
+train2$REKOMENDASI_TINDAK_LANJUT = relevel(train2$REKOMENDASI_TINDAK_LANJUT,ref = "Angsuran Biasa")
+require(nnet)
+multinom_model = multinom(REKOMENDASI_TINDAK_LANJUT ~ ., data=train2)
+
+summary (multinom_model)
+exp (coef(multinom_model))
+head(round(fitted(multinom_model),2))
+train2$ClassPredicted = predict(multinom_model,newdata = train2, "class")
+train_prob = predict(multinom_model,newdata = train2, "probs")
+df = train_prob
+df$max = apply(df,1,max)
+train2$score = df$max
+test_prob = predict(multinom_model, newdata = test, "probs")
+df2 = test_prob
+df2$max=apply(df2,1,max)
+
+tab_train = table(train2$REKOMENDASI_TINDAK_LANJUT,train2$ClassPredicted)
+round((sum(diag(tab_train))/sum(tab_train))*100,4)
+test$ClassPredicted = predict(multinom_model,newdata = test,"class")
+test$score = df2$max
+tab_test=table(test$REKOMENDASI_TINDAK_LANJUT, test$ClassPredicted)
+round((sum(diag(tab_test))/sum(tab_test))*100,4)
+
+
+
+
